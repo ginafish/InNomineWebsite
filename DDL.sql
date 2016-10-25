0b603f6952d3b75dd372d3943807f031be4c2e54
ginafish <phippsg@oregonstate.edu>
2016-10-24 21:42:50 -0700

    Initial commit, beginning of DDL

---
+++ DDL.sql
@@ -0,0 +1,12 @@
+CREATE TABLE Characters (
+	CharacterID INT NOT NULL,
+	CharacterName VARCHAR(30) NOT NULL,
+	Dissonance VARCHAR(15),
+	Discord VARCHAR(15),
+	Superior VARCHAR(20) NOT NULL,
+	ChoirBandMembership VARCHAR(1) NOT NULL,
+	Essence INT NOT NULL
+);
+
+CREATE TABLE Items (
+	
