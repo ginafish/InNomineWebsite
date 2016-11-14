764662e453c8f8961890b883b744117d6452345b
ginafish <phippsg@oregonstate.edu>
2016-11-13 17:37:56 -0800

    Possibly finished player campaign view queries

--- SQL/playerCampaigns.sql
+++ SQL/playerCampaigns.sql
@@ -3,7 +3,10 @@
 SELECT CharacterID, CharacterName, Dissonance, Discord, Superior, ChoirBandMembership, Essence
 FROM Characters
 WHERE Username = $user
-AND CharacterID IN (SELECT CharacterID FROM CharacterCampaignParticipation WHERE CampaignID = $campID)
+AND CharacterID IN
+ (SELECT CharacterID
+  FROM CharacterCampaignParticipation
+  WHERE CampaignID = $campID)
 
 --After that query, save the CharacterID for these queries:
 
@@ -14,6 +17,23 @@ WHERE OwnedSkills.CharacterID = $charID
 
 --Songs
 SELECT OwnedSongs.SongName, OwnedSongs.Force, OwnedSongs.RanksTaken, Songs.EssenceRequired, Songs.Duration, Songs.Disruption, Songs.ShortDescription, Songs.LongDescription
-FROM OwnedSongs JOIN Songs ON OwnedSongs.SongName = Songs.SongName AND OwnedSongs.Force = Songs.Force
+FROM OwnedSongs 
+JOIN Songs 
+ON OwnedSongs.SongName = Songs.SongName 
+  AND OwnedSongs.Force = Songs.Force
 WHERE OwnedSongs.CharacterID = $charID
 
+--Vessels
+SELECT Vessels.VesselName, Vessels.Role, ActiveVessels.CurrentHitPoints, Vessels.HitPoints
+FROM Vessels 
+JOIN ActiveVessels 
+ON Vessels.VesselID = ActiveVessels.VesselID 
+AS Vssls
+WHERE Vssls.CampaignID = $campID
+AND Vssls.CharacterID = $charID
+
+--Items
+SELECT ItemName, Damage, ItemDescription FROM Items WHERE CharacterID = $charID
+
+--HP
+SELECT CurrentMindHits, CurrentSoulHits FROM CharacterCampaignParticipation WHERE CharacterID = $charID AND CampaignID = $campID
