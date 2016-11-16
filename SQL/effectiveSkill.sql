2a281f73ad7460fb302dd9e487a498c650469ab5
Marie <bomberm@oregonstate.edu>
2016-11-15 21:39:55 -0800

    F you mySQL

--- SQL/effectiveSkill.sql
+++ SQL/effectiveSkill.sql
@@ -1,30 +1,28 @@
---Character skills proceedure: Take $characterID and process their skill list and attributes and gives the 'effective skill' for each owned skill
+CREATE DEFINER=`cpses_tu913vF31i`@`localhost` FUNCTION `SkillCalc`(`characterID` INT(1), `skllNm` VARCHAR(20)) RETURNS int(1)
+    DETERMINISTIC
+BEGIN
+DECLARE effectSkl int;
+DECLARE bsSkll VARCHAR(4);
+DECLARE skillStatModify int;
+SET bsSkll = (SELECT baseSkill FROM Skills WHERE SkillName = skllNm);
+SET skillStatModify = 0;
 
-function effectiveSkillCalc (in $characterID, in $skillName)
-returns int
-deterministic
-begin
-	declare effectSkl int;
-	declare bsSkll VARCHAR(4);
-	set bsSkll = (SELECT baseSkill FROM Skills WHERE SkillName = $skillName);
-	
-	declare skillStatModify int;
-	IF (bsSkll = 'STR') THEN
-		set skillStatModify = (SELECT Strength FROM CorporealStats WHERE CharacterID = $characterID);
-	ELSE IF (bsSkll = 'AGI') THEN
-		set skillStatModify = (SELECT Agility FROM CorporealStats WHERE CharacterID = $characterID);
-	ELSE IF (bsSkll = 'INT') THEN
-		set skillStatModify = (SELECT Intelligence FROM EtherealStats WHERE CharacterID = $characterID);
-	ELSE IF (bsSkll = 'PREC') THEN
-		set skillStatModify = (SELECT Prec FROM EtherealStats WHERE CharacterID = $characterID);
-	ELSE IF (bsSkll = 'WILL') THEN
-		set skillStatModify = (SELECT Will FROM CelestialStats WHERE CharacterID = $characterID);
-	ELSE
-		set skillStatModify = (SELECT Perception FROM CelestialStats WHERE CharacterID = $characterID);
-	end if;	
-	
-	set effectSkl = (SELECT DefaultCheck FROM Skill WHERE SkillName = $skillName)
-	
-	set effectSkl = effectSkl + skillStatModify;
-	return effectSkl;
-end;
+IF (bsSkll='STR') THEN
+SET skillStatModify = (SELECT Strength FROM CorporealStats WHERE CoporealStats.CharacterID = characterID);
+ELSEIF (bsSkll = 'AGI') THEN
+SET skillStatModify = (SELECT Agility FROM CorporealStats WHERE CharacterID = characterID);
+ELSEIF (bsSkll = 'INT') THEN
+SET skillStatModify = (SELECT Intelligence FROM EtherealStats WHERE CharacterID = characterID);
+ELSEIF (bsSkll = 'PREC') THEN
+SET skillStatModify = (SELECT Prec FROM EtherealStats WHERE CharacterID = characterID);
+ELSEIF (bsSkll = 'WILL') THEN
+SET skillStatModify = (SELECT Will FROM CelestialStats WHERE CharacterID = characterID);
+ELSE
+SET skillStatModify = (SELECT Perception FROM CelestialStats WHERE CharacterID = characterID);
+END IF;
+
+SET effectSkl = (SELECT DefaultCheck FROM Skills WHERE SkillName = skllNm);
+SET effectSkl = (effectSkl + skillStatModify);
+
+RETURN (effectSkl);
+END
