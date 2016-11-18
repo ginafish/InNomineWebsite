b9cf6eafc8aef6cb7dc9f0c2c8dceb817f33d61d
ginafish <phippsg@oregonstate.edu>
2016-11-17 16:03:05 -0800

    Fixed inserts to have semicolons

--- SQL/characterCreate.sql
+++ SQL/characterCreate.sql
@@ -1,25 +1,25 @@
 INSERT INTO Characters
-VALUES ($characterName, $dissonance, $discord, $superior, $choirBandMem, $essence, $user)
+VALUES ($characterName, $dissonance, $discord, $superior, $choirBandMem, $essence, $user);
 
 /*make this some kind of trigger or something to store CharacterID in a variable, since CharacterID is an autogennumthing???*/
 
 INSERT INTO CorporealStats
-VALUES ($characterID, $str, $agi, $corp)
+VALUES ($characterID, $str, $agi, $corp);
 
 INSERT INTO CelestialStats
-VALUES ($characterID, $will, $perc, $soulhp, $celes)
+VALUES ($characterID, $will, $perc, $soulhp, $celes);
 
 INSERT INTO EtherealStats
-VALUES ($characterID, $int, $prec, $mindhp, $eth)
+VALUES ($characterID, $int, $prec, $mindhp, $eth);
 
 INSERT INTO Vessels
-VALUES ($characterID, $vesselName, $vesselRole, $hitPoints)
+VALUES ($characterID, $vesselName, $vesselRole, $hitPoints);
 
 INSERT INTO Items
-VALUES ($characterID, $itemName, $dmg, $itmdesc)
+VALUES ($characterID, $itemName, $dmg, $itmdesc);
 
 INSERT INTO OwnedSkills
-VALUES ($characterID, $skillName, $ranksTaken)
+VALUES ($characterID, $skillName, $ranksTaken);
 
 INSERT INTO OwnedSongs
-VALUES ($characterID, $songName, $force, $ranksTaken)
+VALUES ($characterID, $songName, $force, $ranksTaken);
