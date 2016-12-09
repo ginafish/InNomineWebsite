f8c33a8ee86d9a4627f7237bc83a0a71f5df1267
Mars Bomber <bomberm@oregonstate.edu>
2016-12-09 02:49:27 -0800

    Ahhh, push it!

--- Webpage/scripts/addCampaign.php
+++ Webpage/scripts/addCampaign.php
@@ -6,9 +6,7 @@ include_once '../includes/functions.php';
 	if(!isset($_POST['campName'])){
 		echo("I'm broken");
 	} else {
-		$submitNew="INSERT INTO Campaigns CampaignName='".$_POST['campName']."', CampaignPassword='".$_POST['campPass']."'";
-		$submitNew.=", CharacterRestrictions='".$_POST['playerRestr']."', CampaignBlurb='".$_POST['campBlurb']."', PlayerLimit='".$_POST['maxPlayers']."'";
-		echo $submitNew;
+		$submitNew="INSERT INTO Campaigns (CampaignName, CampaignPassword, CharacterRestrictions, CampaignBlurb, PlayerLimit, GameMasterUsername) VALUES('".$_POST['campName']."', '".$_POST['campPass']."', '".$_POST['playerRestr']. "', '".$_POST['campBlurb']. "', '".$_POST['maxPlayers']."', '".$_SESSION['username']."')";
 		$result=mysqli_query($mysqli, $submitNew);
 		if($result==1){
 			header('Location: ../../dashboard.php');
